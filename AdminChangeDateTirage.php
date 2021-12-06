38eabe4396ed54f61be010240a3520d0142738d7
MehdiForeach <mehdi.birembaut.foreach@gmail.com>
2021-12-06 15:30:11 +0100

    maj

--- AdminChangeDateTirage.php
+++ AdminChangeDateTirage.php
@@ -1,11 +1,35 @@
 <?php
-// Admin pour régler la date du tirage
+    require_once "ressources.php";
+    require_once "fonctions.php";
+
+    entete("(Admin-MAJ) Date de tirage au sort");
+
+    if($_POST)
+    {
+        $date = $_POST['dateTirage'];
+
+        $mysqli = new mysqli($servername, $username, $password, $database);
+
+        $query  = "UPDATE dateTirage SET dateFin='$date';";
+        // print( $query."<br>");
+        $res = $mysqli->query( $query );
+
+        if($res)
+            print("Mise à jour effectué !");
+
+        else
+        print("Erreur !!");
+
+        $mysqli->close();
+    }
 
 ?>
 
-<form action="#" method="post">
-    Date du tirage au sort
-    <input type="text" name="nom" placeholder="Date">
-    <br>
-    <button type="submit">OK</button>
+<form action="#" method="POST">
+  <label for="dateTirage">Mise à jour de la date du tirage au sort :</label>
+  <input type="datetime-local" id="dateTirage" name="dateTirage">
+  <input type="submit">
 </form>
+
+</body>
+</html>
