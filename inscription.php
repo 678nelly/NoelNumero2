b14ff4cbda0c1bd4cebe3c83d77e4a288752a2e4
MehdiForeach <mehdi.birembaut.foreach@gmail.com>
2021-12-06 12:50:05 +0100

    maj

--- inscription.php
+++ inscription.php
@@ -1,4 +1,33 @@
 <?php
+    require_once "ressources.php";
+    require_once "fonctions.php";
 
+    entete( "Inscription" );
 
+    if ($_POST)
+    {
+        $pseudo            = $_POST[ 'pseudo' ];
+        $mail             = $_POST[ 'mail' ];
+
+        $mysqli = new mysqli($servername, $username, $password, $database);
+
+        $query  = "insert into inscriptions ( pseudo, email ) values ( '$pseudo', '$mail' );";
+        //print( $query );
+        if ( $mysqli->query( $query ) )
+            print( "<h3>$pseudo enregistré</h3>");
+        else
+            print( "<h3>erreur d'enregistrement</h3>");
+        $mysqli->close();
+    }
 ?>
+
+<form action="#" method="POST">
+    <input type="text" name='pseudo' placeholder="Entrez votre pseudo">
+    <br>
+    <input type="text" name='mail' placeholder="Entrez votre mail">
+    <br>
+    <button type="submit">Valider</button>
+</form>
+
+</body>
+</html>
