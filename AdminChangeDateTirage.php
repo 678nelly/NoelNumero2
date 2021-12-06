5895da48591b2e3e914ad62e3c47ec67c4a27768
Nelly <nelly.descamps.foreach@gmail.com>
2021-12-06 14:14:21 +0100

    création page admin pour changer la date

---
+++ AdminChangeDateTirage.php
@@ -0,0 +1,11 @@
+<?php
+// Admin pour régler la date du tirage
+
+?>
+
+<form action="#" method="post">
+    Date du tirage au sort
+    <input type="text" name="nom" placeholder="Date">
+    <br>
+    <button type="submit">OK</button>
+</form>
