fb51970bdd974bef2f13a1c2fe1ef5e669ce9220
Nelly <nelly.descamps.foreach@gmail.com>
2021-12-07 10:00:25 +0100

    modif fonction et inscription + ajout mp4, html et css

--- inscription.php
+++ inscription.php
@@ -4,7 +4,7 @@
 
     entete( "Inscription" );
 
-    if ($_POST)
+    if ($_POST=="toto") //=="toto" -> à supprimer après la fin du CSS
     {
         $pseudo            = $_POST[ 'pseudo' ];
         $mail             = $_POST[ 'mail' ];
@@ -14,7 +14,7 @@
         $query  = "insert into inscriptions ( pseudo, email ) values ( '$pseudo', '$mail' );";
         //print( $query );
         if ( $mysqli->query( $query ) )
-            print( "<h3>$pseudo enregistré</h3>");
+            print( "<h3>Merci $pseudo pour ton inscription !</h3>");
         else
             print( "<h3>erreur d'enregistrement</h3>");
         $mysqli->close();
@@ -26,8 +26,17 @@
     <br>
     <input type="text" name='mail' placeholder="Entrez votre mail">
     <br>
-    <button type="submit">Valider</button>
+    <button class="styleBouton" type="submit">Valider</button>
 </form>
 
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="images/lutinsDeNoel.PNG">
+      <img src="images/lutinsDeNoel.PNG" width="600" height="400">
+    </a>
+    <div class="desc"> Ton cadeau est-il prêt à être offert ? </div>
+  </div>
+</div>
 </body>
 </html>
